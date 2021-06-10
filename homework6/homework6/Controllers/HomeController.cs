3d12f75e9d39a607afc84d6b3db02de9772aebcc
Xiao Ling <lingx@microsoft.com>
2021-06-09 22:07:48 -0400

    added homework6

---
+++ homework6/homework6/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using homework6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homework6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new BirthdayCard());
+        }
+
+        // Receive the data coming from the browser
+        [HttpPost]
+        public IActionResult Index(Models.BirthdayCard guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("SendCard", guestResponse);
+            }
+            else
+            {
+                return View(guestResponse);
+            }
+        }
+
+        public IActionResult SendCard()
+        {
+            return View();
+        }
+    }
+}
